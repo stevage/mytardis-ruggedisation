29ce9c89b6171539aaa269e7aa3a5a49f355132a
Gerson Galang <gerson721@gmail.com>
2010-08-24 02:56:09 +0000

    merged all the changes in trunk (rev 123 to 162) on the mecat branch
    git-svn-id: http://mytardis.googlecode.com/svn/branches/mecat-branch@163 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis_portal/ExperimentParser.py
+++ tardis_portal/ExperimentParser.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
 # Copyright (c) 2010, Monash e-Research Centre
 #   (Monash University, Australia)
@@ -27,151 +29,191 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+
 from lxml import etree
 from StringIO import StringIO
 from django.utils.safestring import SafeUnicode
 
+
 class ExperimentParser:
-	__schema_mods = ('mods', 'http://www.loc.gov/mods/v3')
-	__schema_mets = ('METS', 'http://www.loc.gov/METS/')
-	__schema_xlink = ('xlink', 'http://www.w3.org/1999/xlink')
-	
-	__xpath_findModsElement = "//METS:dmdSec[//METS:div[@TYPE='investigation']/@DMDID = @ID]/METS:mdWrap[@MDTYPE='MODS']"
-	__xpath_findDivElementWithTypeInvestigation = "//METS:structMap/METS:div[@TYPE='investigation']"
-	__xpath_findFileGroup = "//METS:fileSec/METS:fileGrp"
-	
-	def __init__(self, xmlString):
-		self.tree = etree.parse(StringIO(xmlString))
-		print '(Initializing %s)' % self.tree
-	
-	def __getSingleResult(self, elements):
-		if len(elements) == 1:
-			# let's reformat the element value that is not in a continuous line
-			import os
-			return self.__removeLineSeparators(SafeUnicode(elements[0]))
-		else:
-			return None
-	
-	def __removeLineSeparators(self, str):
-		"""Remove all the line separators and whitespaces from the given string."""
-		return " ".join([l.strip() for l in str.splitlines()]).strip()
-	
-	def __getStrippedElements(self, elements):
-		"""Strip the extra whitespaces (eg '\t', ' ', '\n', etc) at the front and back of each of the elements."""
-		return [e.strip() for e in elements]		
 
-	def getTitle(self):
-		elements = self.tree.xpath(self.__xpath_findModsElement + "//mods:title/text()", \
-			namespaces=dict([self.__schema_mods, self.__schema_mets]))
-		return self.__getSingleResult(elements)
+    __schema_mods = ('mods', 'http://www.loc.gov/mods/v3')
+    __schema_mets = ('METS', 'http://www.loc.gov/METS/')
+    __schema_xlink = ('xlink', 'http://www.w3.org/1999/xlink')
 
-	def getAuthors(self):
-		elements = self.tree.xpath(self.__xpath_findModsElement + "//mods:name/mods:namePart/text()", \
-			namespaces=dict([self.__schema_mods, self.__schema_mets]))
-		return self.__getStrippedElements(elements)
+    __xpath_findModsElement = \
+        "//METS:dmdSec[//METS:div[@TYPE='investigation']/@DMDID = @ID]/METS:mdWrap[@MDTYPE='MODS']"
+    __xpath_findDivElementWithTypeInvestigation = \
+        "//METS:structMap/METS:div[@TYPE='investigation']"
+    __xpath_findFileGroup = '//METS:fileSec/METS:fileGrp'
 
-	def getAbstract(self):
-		elements = self.tree.xpath(self.__xpath_findModsElement + "//mods:abstract/text()", \
-			namespaces=dict([self.__schema_mods, self.__schema_mets]))
-		return self.__getSingleResult(elements)
-	
-	def getPDBIDs(self):
-		elements = self.tree.xpath(self.__xpath_findModsElement + "//mods:identifier[@type='pdb']/text()", \
-			namespaces=dict([self.__schema_mods, self.__schema_mets]))
-		return self.__getStrippedElements(elements)
-	
-	def getRelationURLs(self):
-		elements = self.tree.xpath(self.__xpath_findModsElement + "//mods:url/text()", \
-			namespaces=dict([self.__schema_mods, self.__schema_mets]))
-		return self.__getStrippedElements(elements)
-	
-	def getAgentName(self, role):
-		elements = self.tree.xpath("//METS:metsHdr/METS:agent[@ROLE='" + role + "']/METS:name/text()",
-			namespaces=dict([self.__schema_mods, self.__schema_mets]))
-		return self.__getSingleResult(elements)	
-	
-	def getDatasetTitle(self, dataset_id):
-		elements = self.tree.xpath("//METS:dmdSec[@ID='" + dataset_id + "']/METS:mdWrap[@MDTYPE='MODS']" + 
-		    "//mods:title/text()", \
-			namespaces=dict([self.__schema_mods, self.__schema_mets]))
-		return self.__getSingleResult(elements)
-	
-	def getDatasetDMDIDs(self):		
-		"""Get the IDs of the all the datasets of this experiment."""
-		elements = self.tree.xpath(self.__xpath_findDivElementWithTypeInvestigation + 
-		    "//METS:div[@TYPE='dataset']" + "/@DMDID", \
-			namespaces=dict([self.__schema_mets]))
-		return self.__getStrippedElements(elements)
-	
-	def getDatasetADMIDs(self, dataset_id):
-		"""Get the metadata IDs for the given dataset."""
-		elements = self.tree.xpath(self.__xpath_findDivElementWithTypeInvestigation + 
-			"//METS:div[@TYPE='dataset' and @DMDID='" + dataset_id + "']/@ADMID", \
-			namespaces=dict([self.__schema_mets]))
-		return self.__getStrippedElements(elements)
-	
-	def getFileIDs(self, dataset_id):
-		"""Get the list of IDs of files for the given dataset."""
-		elements = self.tree.xpath("//METS:div[@DMDID='" + dataset_id + "']" + "//METS:fptr/@FILEID", \
-			namespaces=dict([self.__schema_mets]))
-		return self.__getStrippedElements(elements)
-	
-	def getFileLocation(self, file_id):
-		elements = self.tree.xpath("//METS:fileSec/METS:fileGrp/METS:file[@ID='" + file_id + "']" + 
-		    "/METS:FLocat/@xlink:href", \
-			namespaces=dict([self.__schema_xlink, self.__schema_mets]))
-		return self.__getSingleResult(elements)
-	
-	def getFileADMIDs(self, file_id):
-		"""Get the metadata IDs for the given file."""
-		elements = self.tree.xpath(self.__xpath_findFileGroup + "/METS:file[@ID='" + file_id + "']/@ADMID",
-			namespaces=dict([self.__schema_mets]))
-		return self.__getStrippedElements(elements)
-	
-	def getFileName(self, file_id):
-		"""Get the filename for the given file_id."""
-		elements = self.tree.xpath(self.__xpath_findFileGroup + "/METS:file[@ID='" + file_id + "']/@OWNERID",
-			namespaces=dict([self.__schema_mets]))
-		return self.__getSingleResult(elements)
+    def __init__(self, xmlString):
+        self.tree = etree.parse(StringIO(xmlString))
+        print '(Initializing %s)' % self.tree
 
-	def getFileSize(self, file_id):
-		elements = self.tree.xpath(self.__xpath_findFileGroup + "/METS:file[@ID='" + file_id + "']/@SIZE",
-			namespaces=dict([self.__schema_mets]))
-		return self.__getSingleResult(elements)
-	
-	def getTechXML(self, tech_id):
-		"""Get the datafile element which holds the metadata for the file given the metadata ID.
-		
+    def __getSingleResult(self, elements):
+        if len(elements) == 1:
+
+            # let's reformat the element value that is not in a continuous line
+            import os
+            return self.__removeLineSeparators(SafeUnicode(elements[0]))
+        else:
+            return None
+
+    def __removeLineSeparators(self, str):
+        """Remove all the line separators and whitespaces from the given string."""
+
+        return ' '.join([l.strip() for l in str.splitlines()]).strip()
+
+    def __getStrippedElements(self, elements):
+        """Strip the extra whitespaces (eg '\t', ' ', '\n', etc) at the front and back of each of the elements."""
+
+        return [e.strip() for e in elements]
+
+    def getTitle(self):
+        elements = self.tree.xpath(self.__xpath_findModsElement +
+                                   '//mods:title/text()',
+                                   namespaces=dict([self.__schema_mods,
+                                   self.__schema_mets]))
+        return self.__getSingleResult(elements)
+
+    def getAuthors(self):
+        elements = self.tree.xpath(self.__xpath_findModsElement +
+                                   '//mods:name/mods:namePart/text()',
+                                   namespaces=dict([self.__schema_mods,
+                                   self.__schema_mets]))
+        return self.__getStrippedElements(elements)
+
+    def getAbstract(self):
+        elements = self.tree.xpath(self.__xpath_findModsElement +
+                                   '//mods:abstract/text()',
+                                   namespaces=dict([self.__schema_mods,
+                                   self.__schema_mets]))
+        return self.__getSingleResult(elements)
+
+    def getPDBIDs(self):
+        elements = self.tree.xpath(self.__xpath_findModsElement +
+                                   "//mods:identifier[@type='pdb']/text()",
+                                   namespaces=dict([self.__schema_mods,
+                                   self.__schema_mets]))
+        return self.__getStrippedElements(elements)
+
+    def getRelationURLs(self):
+        elements = self.tree.xpath(self.__xpath_findModsElement +
+                                   '//mods:url/text()',
+                                   namespaces=dict([self.__schema_mods,
+                                   self.__schema_mets]))
+        return self.__getStrippedElements(elements)
+
+    def getAgentName(self, role):
+        elements = self.tree.xpath("//METS:metsHdr/METS:agent[@ROLE='" +
+                                   role + "']/METS:name/text()",
+                                   namespaces=dict([self.__schema_mods,
+                                   self.__schema_mets]))
+        return self.__getSingleResult(elements)
+
+    def getDatasetTitle(self, dataset_id):
+        elements = self.tree.xpath("//METS:dmdSec[@ID='" + dataset_id +
+                                   "']/METS:mdWrap[@MDTYPE='MODS']" +
+                                   '//mods:title/text()',
+                                   namespaces=dict([self.__schema_mods,
+                                   self.__schema_mets]))
+        return self.__getSingleResult(elements)
+
+    def getDatasetDMDIDs(self):
+        """Get the IDs of the all the datasets of this experiment."""
+
+        elements = \
+            self.tree.xpath(self.__xpath_findDivElementWithTypeInvestigation +
+                            "//METS:div[@TYPE='dataset']" + '/@DMDID',
+                            namespaces=dict([self.__schema_mets]))
+        return self.__getStrippedElements(elements)
+
+    def getDatasetADMIDs(self, dataset_id):
+        """Get the metadata IDs for the given dataset."""
+
+        elements = \
+            self.tree.xpath(self.__xpath_findDivElementWithTypeInvestigation +
+                            "//METS:div[@TYPE='dataset' and @DMDID='" +
+                            dataset_id + "']/@ADMID",
+                            namespaces=dict([self.__schema_mets]))
+        return self.__getStrippedElements(elements)
+
+    def getFileIDs(self, dataset_id):
+        """Get the list of IDs of files for the given dataset."""
+
+        elements = self.tree.xpath("//METS:div[@DMDID='" + dataset_id +
+                                   "']" + '//METS:fptr/@FILEID',
+                                   namespaces=dict([self.__schema_mets]))
+        return self.__getStrippedElements(elements)
+
+    def getFileLocation(self, file_id):
+        elements = \
+            self.tree.xpath("//METS:fileSec/METS:fileGrp/METS:file[@ID='" +
+                            file_id + "']" + '/METS:FLocat/@xlink:href',
+                            namespaces=dict([self.__schema_xlink,
+                            self.__schema_mets]))
+        return self.__getSingleResult(elements)
+
+    def getFileADMIDs(self, file_id):
+        """Get the metadata IDs for the given file."""
+
+        elements = self.tree.xpath(self.__xpath_findFileGroup +
+                                   "/METS:file[@ID='" + file_id +
+                                   "']/@ADMID",
+                                   namespaces=dict([self.__schema_mets]))
+        return self.__getStrippedElements(elements)
+
+    def getFileName(self, file_id):
+        """Get the filename for the given file_id."""
+
+        elements = self.tree.xpath(self.__xpath_findFileGroup +
+                                   "/METS:file[@ID='" + file_id +
+                                   "']/@OWNERID",
+                                   namespaces=dict([self.__schema_mets]))
+        return self.__getSingleResult(elements)
+
+    def getFileSize(self, file_id):
+        elements = self.tree.xpath(self.__xpath_findFileGroup +
+                                   "/METS:file[@ID='" + file_id +
+                                   "']/@SIZE",
+                                   namespaces=dict([self.__schema_mets]))
+        return self.__getSingleResult(elements)
+
+    def getTechXML(self, tech_id):
+        """Get the datafile element which holds the metadata for the file given the metadata ID.
+
 		Arguments:
 		tech_id -- the metadata ID
 		
 		Returns:
 		The datafile element which holds the metadata for the file given the metadata ID.
-		
-		"""				
-		f = StringIO("<?xml version='1.0' ?>" + 
-		"<xsl:stylesheet version='2.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform' xmlns:mets='http://www.loc.gov/METS/'>" +
-		"<xsl:output method='xml' indent='yes'/>" + 
-		"<xsl:template match='/'>" + "<xsl:copy-of select=\"//mets:techMD[@ID='" + 
-		tech_id +
-		"']/mets:mdWrap/mets:xmlData/*\"/>" +
-		"</xsl:template></xsl:stylesheet>")
-		xslt_doc = etree.parse(f)
-		transform = etree.XSLT(xslt_doc)
-		result_tree = transform(self.tree)
-		return result_tree
-	
-	def getParameterFromTechXML(self, tech_xml, parameter_name):
-		prefix = tech_xml.getroot().prefix
-		xmlns = tech_xml.getroot().nsmap[prefix]
-	
-		parameter_string = ""
-		for parameter in parameter_name.split('/'):
-			parameter_string = parameter_string + "/" + prefix + ":" + parameter
-		
-		elements = tech_xml.xpath("/" + parameter_string + "/text()",
-			namespaces={prefix: xmlns})
-		return self.__getSingleResult(elements)
+
+		"""
+
+        f = StringIO("<?xml version='1.0' ?>"
+                     + "<xsl:stylesheet version='2.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform' xmlns:mets='http://www.loc.gov/METS/'>"
+                      + "<xsl:output method='xml' indent='yes'/>"
+                     + "<xsl:template match='/'>"
+                     + '<xsl:copy-of select="//mets:techMD[@ID=\''
+                     + tech_id + '\']/mets:mdWrap/mets:xmlData/*"/>'
+                     + '</xsl:template></xsl:stylesheet>')
+        xslt_doc = etree.parse(f)
+        transform = etree.XSLT(xslt_doc)
+        result_tree = transform(self.tree)
+        return result_tree
+
+    def getParameterFromTechXML(self, tech_xml, parameter_name):
+        prefix = tech_xml.getroot().prefix
+        xmlns = tech_xml.getroot().nsmap[prefix]
+
+        parameter_string = ''
+        for parameter in parameter_name.split('/'):
+            parameter_string = parameter_string + '/' + prefix + ':' \
+                + parameter
+
+        elements = tech_xml.xpath('/' + parameter_string + '/text()',
+                                  namespaces={prefix: xmlns})
+        return self.__getSingleResult(elements)
 
 
-	
--- tardis_portal/tests.py
+++ tardis_portal/tests.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
 # Copyright (c) 2010, Monash e-Research Centre
 #   (Monash University, Australia)
@@ -28,34 +30,40 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-
 # http://docs.djangoproject.com/en/dev/topics/testing/
+
 from django.test import TestCase
 from django.test.client import Client
 import unittest
 
+
 class SimpleTest(TestCase):
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
+
         self.failUnlessEqual(1 + 1, 2)
 
 
 # http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
 class UserInterfaceTestCase(TestCase):
+
     def test_root(self):
         self.failUnlessEqual(Client().get('/').status_code, 200)
 
     def test_urls(self):
         c = Client()
-        urls  = ['/login', '/about', '/partners', '/stats']
+        urls = ['/login', '/about', '/partners', '/stats']
         urls += ['/experiment/register', '/experiment/view']
         urls += ['/search/experiment', '/search/datafile']
 
         for u in urls:
             response = c.get(u)
+
             # print u, response.status_code
+
             self.failUnlessEqual(response.status_code, 301)
 
     def test_register(self):
@@ -64,104 +72,160 @@ class UserInterfaceTestCase(TestCase):
         from django.contrib.auth.models import User
         from tardis import settings
         import os
-        
-        user='user1'
-        pwd='test'
-        email=''
+
+        user = 'user1'
+        pwd = 'test'
+        email = ''
         User.objects.create_user(user, email, pwd)
 
-        f = open(os.path.join(settings.APP_ROOT, "tardis_portal/tests/notMETS_test.xml"), "r")
-        response = self.client.post('/experiment/register/', {'username': user,
-                                                              'password': pwd,
-                                                              'xmldata': f,
-                                                              'originid': '286',
-                                                              'experiment_owner': user})
+        f = open(os.path.join(settings.APP_ROOT,
+                 'tardis_portal/tests/notMETS_test.xml'), 'r')
+        response = self.client.post('/experiment/register/', {
+            'username': user,
+            'password': pwd,
+            'xmldata': f,
+            'originid': '286',
+            'experiment_owner': user,
+            })
         f.close()
         self.failUnlessEqual(response.status_code, 200)
-    
+
     def test_login(self):
         from django.contrib.auth.models import User
-        user='user2'
-        pwd='test'
-        email=''
+        user = 'user2'
+        pwd = 'test'
+        email = ''
         User.objects.create_user(user, email, pwd)
 
-        self.failUnlessEqual(self.client.login(username=user, password=pwd), True)        
-        
+        self.failUnlessEqual(self.client.login(username=user,
+                             password=pwd), True)
+
+
 class ExperimentParserTestCase(unittest.TestCase):
+
     def setUp(self):
         from tardis import settings
         import os
-        f = open(os.path.join(settings.APP_ROOT, "tardis_portal/tests/METS_test.xml"), "r")
+        f = open(os.path.join(settings.APP_ROOT,
+                 'tardis_portal/tests/METS_test.xml'), 'r')
         xmlString = f.read()
         f.close()
         from tardis.tardis_portal.ExperimentParser import ExperimentParser
         self.experimentParser = ExperimentParser(str(xmlString))
 
     def testGetTitle(self):
-        self.assertTrue(self.experimentParser.getTitle() == 'Test Title', 'title is not the same')
+        self.assertTrue(self.experimentParser.getTitle() == 'Test Title'
+                        , 'title is not the same')
 
     def testGetAuthors(self):
-        self.assertTrue(len(self.experimentParser.getAuthors()) == 3, 'number of authors should be 3' )
-        self.assertTrue('Author2' in self.experimentParser.getAuthors(), '"Author2 should be in the authors list"' )
-        
-    def testGetAbstract(self):
-        self.assertTrue(self.experimentParser.getAbstract() == 'Test Abstract.', 'abstract is not the same')
-    
-    def testGetPDBIDs(self):        
-        self.assertTrue(len(self.experimentParser.getPDBIDs()) == 1, 'number of pdb ids should be 1')
-        self.assertTrue('SAMPLE_PDBID' in self.experimentParser.getPDBIDs(), 'pdb id should be "SAMPLE_PDBID"')
-    
-    def testGetRelationURLs(self):
-        self.assertTrue('http://www.test.com' in self.experimentParser.getRelationURLs(), 'missing url from relationsURLs')
-        self.assertTrue(len(self.experimentParser.getRelationURLs()) == 1, 'there should only be 1 relationsURL')
-    
-    def testGetAgentName(self):
-        self.assertTrue(self.experimentParser.getAgentName('CREATOR') == 'Creator', 'agent should be "Creator"')
-        self.assertTrue(self.experimentParser.getAgentName('PAINTER') == None, 'there is no "Painter" agent')
-    
-    def testGetDatasetTitle(self):
-        self.assertTrue(self.experimentParser.getDatasetTitle('J-2') == 'Dataset1 Title', 'dataset title should be "J-2"')
-        self.assertTrue(self.experimentParser.getDatasetTitle('J-4') == None, 'there is no dataset with id "J-4"')
-    
-    def testGetDatasetDMDIDs(self):
-        self.assertTrue(len(self.experimentParser.getDatasetDMDIDs()) == 2, 'total number of datasets is wrong')
-        self.assertTrue('J-2' in self.experimentParser.getDatasetDMDIDs(), 'J-2 is not in the dataset')
-        self.assertTrue('J-1' not in self.experimentParser.getDatasetDMDIDs(), "J-1 shouldn't be in the dataset")
-    
-    def testGetDatasetADMIDs(self):
-        # get metadata ids for this dataset...
-        pass
-    
-    def testGetFileIDs(self):
-        self.assertTrue('F-3' in self.experimentParser.getFileIDs('J-3'), 'F-3 is missing from the file IDs')
-        self.assertTrue(len(self.experimentParser.getFileIDs('J-3')) == 2, 'there should only be 2 files for the J-3 dataset')
-        self.assertTrue('F-5' not in self.experimentParser.getFileIDs('J-3'), 'F-3 is missing from the file IDs')
-    
-    def testGetFileLocation(self):
-        self.assertTrue(self.experimentParser.getFileLocation('F-1') == 'file://Images/File1', "file F-1's location is wrong")
-    
-    def testGetFileADMIDs(self):
-        # get metadata ids for this file...
-        self.assertTrue('A-3' in self.experimentParser.getFileADMIDs('F-4'), 'wrong file metadata id')
-        self.assertTrue(len(self.experimentParser.getFileADMIDs('F-4')) == 1, 'there should only be 1 metadata ID for the file')
-    
-    def testGetFileName(self):
-        self.assertTrue(self.experimentParser.getFileName('F-1') == 'File1', 'wrong file name for file "F-1"')
+        self.assertTrue(len(self.experimentParser.getAuthors()) == 3,
+                        'number of authors should be 3')
+        self.assertTrue('Author2'
+                        in self.experimentParser.getAuthors(),
+                        '"Author2 should be in the authors list"')
 
-    def testGetFileSize(self):        
-        self.assertTrue(self.experimentParser.getFileSize('F-1') == '6148', 'wrong file size for file "F-1"')
-    
+    def testGetAbstract(self):
+        self.assertTrue(self.experimentParser.getAbstract()
+                        == 'Test Abstract.', 'abstract is not the same')
+
+    def testGetPDBIDs(self):
+        self.assertTrue(len(self.experimentParser.getPDBIDs()) == 1,
+                        'number of pdb ids should be 1')
+        self.assertTrue('SAMPLE_PDBID'
+                        in self.experimentParser.getPDBIDs(),
+                        'pdb id should be "SAMPLE_PDBID"')
+
+    def testGetRelationURLs(self):
+        self.assertTrue('http://www.test.com'
+                        in self.experimentParser.getRelationURLs(),
+                        'missing url from relationsURLs')
+        self.assertTrue(len(self.experimentParser.getRelationURLs())
+                        == 1, 'there should only be 1 relationsURL')
+
+    def testGetAgentName(self):
+        self.assertTrue(self.experimentParser.getAgentName('CREATOR')
+                        == 'Creator', 'agent should be "Creator"')
+        self.assertTrue(self.experimentParser.getAgentName('PAINTER')
+                        == None, 'there is no "Painter" agent')
+
+    def testGetDatasetTitle(self):
+        self.assertTrue(self.experimentParser.getDatasetTitle('J-2')
+                        == 'Dataset1 Title',
+                        'dataset title should be "J-2"')
+        self.assertTrue(self.experimentParser.getDatasetTitle('J-4')
+                        == None, 'there is no dataset with id "J-4"')
+
+    def testGetDatasetDMDIDs(self):
+        self.assertTrue(len(self.experimentParser.getDatasetDMDIDs())
+                        == 2, 'total number of datasets is wrong')
+        self.assertTrue('J-2'
+                        in self.experimentParser.getDatasetDMDIDs(),
+                        'J-2 is not in the dataset')
+        self.assertTrue('J-1'
+                        not in self.experimentParser.getDatasetDMDIDs(),
+                        "J-1 shouldn't be in the dataset")
+
+    def testGetDatasetADMIDs(self):
+
+        # get metadata ids for this dataset...
+
+        pass
+
+    def testGetFileIDs(self):
+        self.assertTrue('F-3' in self.experimentParser.getFileIDs('J-3'
+                        ), 'F-3 is missing from the file IDs')
+        self.assertTrue(len(self.experimentParser.getFileIDs('J-3'))
+                        == 2,
+                        'there should only be 2 files for the J-3 dataset'
+                        )
+        self.assertTrue('F-5'
+                        not in self.experimentParser.getFileIDs('J-3'),
+                        'F-3 is missing from the file IDs')
+
+    def testGetFileLocation(self):
+        self.assertTrue(self.experimentParser.getFileLocation('F-1')
+                        == 'file://Images/File1',
+                        "file F-1's location is wrong")
+
+    def testGetFileADMIDs(self):
+
+        # get metadata ids for this file...
+
+        self.assertTrue('A-3'
+                        in self.experimentParser.getFileADMIDs('F-4'),
+                        'wrong file metadata id')
+        self.assertTrue(len(self.experimentParser.getFileADMIDs('F-4'))
+                        == 1,
+                        'there should only be 1 metadata ID for the file'
+                        )
+
+    def testGetFileName(self):
+        self.assertTrue(self.experimentParser.getFileName('F-1')
+                        == 'File1', 'wrong file name for file "F-1"')
+
+    def testGetFileSize(self):
+        self.assertTrue(self.experimentParser.getFileSize('F-1')
+                        == '6148', 'wrong file size for file "F-1"')
+
     def testGetTechXML(self):
+
         # check if the root of the returned element is datafile or something else
-        self.assertTrue(
-            self.experimentParser.getTechXML('A-3').getroot().tag == '{http://www.tardis.edu.au/schemas/trdDatafile/1}datafile', 'element has wrong tag')
-    
+
+        self.assertTrue(self.experimentParser.getTechXML('A-3'
+                        ).getroot().tag
+                        == '{http://www.tardis.edu.au/schemas/trdDatafile/1}datafile'
+                        , 'element has wrong tag')
+
     def testGetParameterFromTechXML(self):
         pass
 
+
 def suite():
-    userInterfaceSuite = unittest.TestLoader().loadTestsFromTestCase(UserInterfaceTestCase)
-    parserSuite = unittest.TestLoader().loadTestsFromTestCase(ExperimentParserTestCase)
+    userInterfaceSuite = \
+        unittest.TestLoader().loadTestsFromTestCase(UserInterfaceTestCase)
+    parserSuite = \
+        unittest.TestLoader().loadTestsFromTestCase(ExperimentParserTestCase)
     allTests = unittest.TestSuite([parserSuite, userInterfaceSuite])
     return allTests
+
+
