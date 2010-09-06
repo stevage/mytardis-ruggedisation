0f36775b52bc8ac4d11ba7a66659940fb1aaf6ab
Gerson Galang <gerson721@gmail.com>
2010-09-06 00:52:58 +0000

    forgot to commit this module on my last commit (r179)
    git-svn-id: http://mytardis.googlecode.com/svn/branches/mecat-branch@185 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

---
+++ tardis/tardis_portal/errors.py
@@ -0,0 +1,16 @@
+'''
+Created on 02/09/2010
+
+@author: gerson
+'''
+
+
+class UnsupportedSearchQueryTypeError(Exception):
+
+    def __init__(self, msg):
+        Exception.__init__(self, msg)
+
+class SearchQueryTypeUnprovidedError(Exception):
+
+    def __init__(self, msg):
+        Exception.__init__(self, msg)
