14a153d34ab350333f585416c81521172ab281ff
Kieran Spear <kispear@gmail.com>
2011-08-01 00:52:05 +0000

    Merge in fixes for problems found at ansto deployment.
    git-svn-id: http://mytardis.googlecode.com/svn/branches/htmlabstract-292@1725 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/templatetags/formfieldfilters.py
+++ tardis/tardis_portal/templatetags/formfieldfilters.py
@@ -28,6 +28,8 @@ def parametername_form(value):
 @register.filter
 def sanitize_html(html, bad_tags=['body']):
    """Removes identified malicious HTML content from the given string."""
+   if html is None or html == '':
+      return html
    cleaner = Cleaner(style=False, page_structure=True, remove_tags=bad_tags,
          safe_attrs_only=False)
    return cleaner.clean_html(html)
